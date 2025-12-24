4073679cf6e9988542c556f73801965f9a5db5ba
Ahmad Zainul <126046816+ZainulIzy@users.noreply.github.com>
2025-12-24 23:00:37 +0700

    Update mainScript.lua

--- mainScript.lua
+++ mainScript.lua
@@ -1,118 +1,116 @@
--- [[ TOLHUB - ORION EDITION STABLE ]]
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+-- [[ TOLHUB SCRIPT - RAYFIELD EDITION ]]
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local Window = OrionLib:MakeWindow({
-    Name = "TolHub 🌿", 
-    HidePremium = false, 
-    SaveConfig = true, 
-    ConfigFolder = "TolHub"
+local Window = Rayfield:CreateWindow({
+   Name = "TolHub 🌿",
+   LoadingTitle = "TolHub Script",
+   LoadingSubtitle = "by Xizzy",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = "TolHubData" 
+   },
+   KeySystem = false 
 })
 
--- Variabel Penampung Data
+-- Variabel Penampung Angka
 _G.SpeedInput = 16
 _G.JumpInput = 50
 
 -- [[ TAB MAIN ]]
-local Tab1 = Window:MakeTab({
-	Name = "Main 🏠",
-	Icon = "rbxassetid://4483362458",
-	PremiumOnly = false
+local MainTab = Window:CreateTab("Main 🏠", 4483362458)
+
+MainTab:CreateSection("Karakter Settings")
+
+-- Input Box untuk Speed
+MainTab:CreateInput({
+   Name = "Set WalkSpeed",
+   PlaceholderText = "Ketik angka (Contoh: 100)",
+   RemoveTextAfterFocusLost = false,
+   Callback = function(Text)
+      _G.SpeedInput = tonumber(Text)
+   end,
 })
 
-Tab1:AddSection({
-	Name = "Karakter Settings"
+-- Tombol Aktifkan Speed
+MainTab:CreateButton({
+   Name = "Aktifkan Speed",
+   Callback = function()
+      local char = game.Players.LocalPlayer.Character
+      if char and char:FindFirstChild("Humanoid") and _G.SpeedInput then
+         char.Humanoid.WalkSpeed = _G.SpeedInput
+         Rayfield:Notify({
+            Title = "Sukses!",
+            Content = "Speed diatur ke: " .. _G.SpeedInput,
+            Duration = 2,
+            Image = 4483362458,
+         })
+      end
+   end,
 })
 
--- Input untuk angka Speed
-Tab1:AddTextbox({
-	Name = "Set WalkSpeed",
-	Default = "16",
-	TextDisappear = false,
-	Callback = function(Value)
-		_G.SpeedInput = tonumber(Value)
-	end	  
+-- Input Box untuk Jump
+MainTab:CreateInput({
+   Name = "Set JumpPower",
+   PlaceholderText = "Ketik angka (Contoh: 100)",
+   RemoveTextAfterFocusLost = false,
+   Callback = function(Text)
+      _G.JumpInput = tonumber(Text)
+   end,
 })
 
--- Tombol untuk aktifin Speed
-Tab1:AddButton({
-	Name = "Aktifkan Speed",
-	Callback = function()
-        if _G.SpeedInput then
-            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = _G.SpeedInput
-            OrionLib:MakeNotification({
-                Name = "Sukses!",
-                Content = "Speed diatur ke: " .. _G.SpeedInput,
-                Time = 2
-            })
-        end
-  	end    
-})
-
--- Input untuk angka Jump
-Tab1:AddTextbox({
-	Name = "Set JumpPower",
-	Default = "50",
-	TextDisappear = false,
-	Callback = function(Value)
-		_G.JumpInput = tonumber(Value)
-	end	  
-})
-
--- Tombol untuk aktifin Jump
-Tab1:AddButton({
-	Name = "Aktifkan Jump",
-	Callback = function()
-        if _G.JumpInput then
-            game.Players.LocalPlayer.Character.Humanoid.JumpPower = _G.JumpInput
-        end
-  	end    
+-- Tombol Aktifkan Jump
+MainTab:CreateButton({
+   Name = "Aktifkan Jump",
+   Callback = function()
+      local char = game.Players.LocalPlayer.Character
+      if char and char:FindFirstChild("Humanoid") and _G.JumpInput then
+         char.Humanoid.JumpPower = _G.JumpInput
+      end
+   end,
 })
 
 -- [[ TAB FARM ]]
-local Tab2 = Window:MakeTab({
-	Name = "Farm 🚜",
-	Icon = "rbxassetid://4483362458",
-	PremiumOnly = false
-})
+local FarmTab = Window:CreateTab("Farm 🚜", 4483362458)
 
-Tab2:AddSection({
-	Name = "Automation"
-})
-
--- Auto Clicker
 local clicking = false
-Tab2:AddToggle({
-	Name = "Auto Clicker",
-	Default = false,
-	Callback = function(Value)
-		clicking = Value
-        task.spawn(function()
-            while clicking do
-                local vim = game:GetService("VirtualInputManager")
-                vim:SendMouseButtonEvent(0, 0, 0, true, game, 0)
-                vim:SendMouseButtonEvent(0, 0, 0, false, game, 0)
-                task.wait(0.1)
-            end
-        end)
-	end    
+FarmTab:CreateToggle({
+   Name = "Auto Clicker",
+   CurrentValue = false,
+   Flag = "AutoClick",
+   Callback = function(Value)
+      clicking = Value
+      task.spawn(function()
+         while clicking do
+            local vim = game:GetService("VirtualInputManager")
+            vim:SendMouseButtonEvent(0, 0, 0, true, game, 0)
+            vim:SendMouseButtonEvent(0, 0, 0, false, game, 0)
+            task.wait(0.1)
+         end
+      end)
+   end,
 })
 
--- Anti-AFK
-Tab2:AddToggle({
-	Name = "Anti-AFK",
-	Default = false,
-	Callback = function(Value)
-        _G.AntiAFK = Value
-        local vu = game:GetService("VirtualUser")
-        game:GetService("Players").LocalPlayer.Idled:Connect(function()
-            if _G.AntiAFK then
-                vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
-                task.wait(1)
-                vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
-            end
-        end)
-	end    
+FarmTab:CreateToggle({
+   Name = "Anti-AFK",
+   CurrentValue = false,
+   Flag = "AntiAFK",
+   Callback = function(Value)
+      _G.AntiAFK = Value
+      local vu = game:GetService("VirtualUser")
+      game:GetService("Players").LocalPlayer.Idled:connect(function()
+         if _G.AntiAFK then
+            vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+            task.wait(1)
+            vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+         end
+      end)
+   end,
 })
 
--- Memberitahu library bahwa setup selesai
-OrionLib:Init()
+-- Notifikasi Akhir
+Rayfield:Notify({
+   Title = "NathHub Ready!",
+   Content = "Menu Rayfield siap digeser, Bos!",
+   Duration = 5,
+   Image = 4483362458,
+})
