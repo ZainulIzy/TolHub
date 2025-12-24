1bbd0d353a9387379f5b90c89f0aea7fd8e690ee
Ahmad Zainul <126046816+ZainulIzy@users.noreply.github.com>
2025-12-24 21:11:27 +0700

    Update mainScript.lua

--- mainScript.lua
+++ mainScript.lua
@@ -1,7 +1,6 @@
--- 1. Panggil jeroannya dulu
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+-- Pastikan kodenya bersih dari karakter aneh
+local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Orion/main/source'))()
 
--- 2. Bikin Window Utama
 local Window = OrionLib:MakeWindow({
     Name = "NathHub Garden", 
     HidePremium = false, 
@@ -10,44 +9,48 @@ local Window = OrionLib:MakeWindow({
     IntroText = "Selamat Datang Bos!"
 })
 
--- 3. Tambahin Tab (Nama variabelnya dibedain: MainTab, FarmTab, dll)
+-- Halaman Utama
 local MainTab = Window:MakeTab({
     Name = "Main",
     Icon = "rbxassetid://4483362458",
     PremiumOnly = false
 })
 
+-- Halaman Farm
 local FarmTab = Window:MakeTab({
     Name = "Farm",
     Icon = "rbxassetid://4483362458",
     PremiumOnly = false
 })
 
+-- Halaman Pet
 local PetTab = Window:MakeTab({
     Name = "Pet",
     Icon = "rbxassetid://4483362458",
     PremiumOnly = false
 })
 
+-- Halaman Webhook
 local WebhookTab = Window:MakeTab({
     Name = "Webhook",
     Icon = "rbxassetid://4483362458",
     PremiumOnly = false
 })
 
--- 4. CONTOH CARA NGITSI FITURNYA (Biar gak nyasar)
+-- Fitur Speed
 MainTab:AddSlider({
-	Name = "Speed",
-	Min = 16,
-	Max = 100,
-	Default = 16,
-	Color = Color3.fromRGB(255,255,255),
-	Increment = 1,
-	ValueName = "Speed",
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-	end    
+    Name = "Speed",
+    Min = 16,
+    Max = 300, -- Gue naikin dikit biar berasa kencengnya
+    Default = 16,
+    Color = Color3.fromRGB(255,255,255),
+    Increment = 1,
+    ValueName = "Speed",
+    Callback = function(Value)
+        if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+        end
+    end    
 })
 
--- 5. Akhiri dengan Init biar muncul
 OrionLib:Init()
